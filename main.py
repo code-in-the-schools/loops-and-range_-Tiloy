da7d005d7937808c9e9a57fd0aa985ea6f08e0d7
Tiloy84 <tiloy67@gmail.com>
2020-07-16 17:13:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+range(1) function
+range(2) function
+range(3) function
+range(4) function
+range(5) function
+range(6) function
+range(7) function
+range(8) function
+range(9) function
+range(10)function
+range(5) function
+range(6)
+range(7)
+range(8)
+range(9)
+range(10)
+range(11)
+range(12)
+range(13)
+range(14)
+range(15)
+range(16)
+range(17)
+range(18)
+range(19)
+range(20)
+range(21)
+range(22)
+range(23)
+range(24)
+range(25)
+range(26)
+range(27)
+range(28)
+range(29) 
+Range(30)
